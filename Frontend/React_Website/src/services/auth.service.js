9fe5e38808f457a43f79b6c35b7a01625e0dc0c4
jesusfb <jfigueroa.beltran@gmail.com>
2023-08-11 15:31:49 -0700

    Update auth.service.js

--- Frontend/React_Website/src/services/auth.service.js
+++ Frontend/React_Website/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth/";
+const API_URL = "https://api-gym-j8nk.onrender.com/api/auth/";
 
 class AuthService {
   login(user_email, password) {

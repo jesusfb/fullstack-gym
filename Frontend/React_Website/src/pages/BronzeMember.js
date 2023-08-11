fa9fa2a30af26ed55ccb77b04a0d4545f6fbc551
jesusfb <jfigueroa.beltran@gmail.com>
2023-08-11 15:33:53 -0700

    Update BronzeMember.js

--- Frontend/React_Website/src/pages/BronzeMember.js
+++ Frontend/React_Website/src/pages/BronzeMember.js
@@ -7,7 +7,7 @@ const BronzeMember = () => {
     const [courses, setCourse] = useState([]);
 
     const loadCourses=async()=> {
-      const result = await axios.get("http://localhost:8080/api/plans/courses/all?plan_id=3");
+      const result = await axios.get("https://api-gym-j8nk.onrender.com/api/plans/courses/all?plan_id=3");
       setCourse(result.data);
     }
 

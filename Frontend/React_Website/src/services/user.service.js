df03f3ca5b86a6d4f92b57876ce6ea5cfecddd6f
jesusfb <jfigueroa.beltran@gmail.com>
2023-08-11 15:31:38 -0700

    Update user.service.js

--- Frontend/React_Website/src/services/user.service.js
+++ Frontend/React_Website/src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'http://localhost:8080/api/';
+const API_URL = 'https://api-gym-j8nk.onrender.com/api/';
 
 class UserService {  
 
